35276f73871857d75338948567d2f78fb7b91be6
rifqyyusuf12 <rifqy@importir.co>
2022-02-26 14:20:39 +0700

    membuat table dari database

---
+++ src/components/movies.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Movies extends Component {
+    state = { 
+        movies: getMovies()
+     } 
+
+     handleDelete= (movie) => {
+         const movies = this.state.movies.filter(m => m._id !== movie._id)
+        this.setState({ movies: movies })
+
+     };
+
+    render() { 
+
+        if(this.state.movies.length === 0)
+        return <p>there are no movies in the database.</p>
+
+        return ( 
+            <React.Fragment>
+                <p>showing {this.state.movies.length} movies in the database.</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                        </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Movies;
