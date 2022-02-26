75071f34d67b58d251d8422fcfb7babf7a637ab3
rifqyyusuf12 <rifqy@importir.co>
2022-02-26 16:08:54 +0700

    menambahkan like

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
+//input : boolean
+//output : onClick
+
+// const Like = props => { 
+//     let classes = "fa fa-heart";
+//     if (!props.liked) classes += "-o"; //apabila this props liked itu false akan nambah -o
+//         return (
+//         <i 
+//         onClick={props.onClick} 
+//         style={{ cursor: "pointer" }}
+//          className={classes} 
+//          aria-hidden="true"
+//          />
+//         );
+//         };
+
+//         export default Like;
+ 
+        //perbedaan mengggunakan sfc = fungtional (diatas) dan cc = class (dibawah)
+
+
 class Like extends Component {
-    state = {  } 
     render() { 
-        return ();
+        let classes = "fa fa-heart";
+        if (!this.props.liked) classes += "-o"; //apabila this props liked itu false akan nambah -o
+        return (
+        <i 
+        onClick={this.props.onClick} 
+        style={{ cursor: "pointer", color: "red"}}
+         className={classes} 
+         aria-hidden="true"
+         />
+        );
     }
 }
  
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/like';
 
 class Movies extends Component {
     state = { 
         movies: getMovies()
-     } 
+     };
 
      handleDelete= (movie) => {
          const movies = this.state.movies.filter(m => m._id !== movie._id)
         this.setState({ movies: movies })
 
      };
+
+
+     handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = {...movies[index]};
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies })
+     }
 
     render() { 
 
@@ -27,6 +37,8 @@ class Movies extends Component {
                             <th>Genre</th>
                             <th>Stock</th>
                             <th>Rate</th>
+                            <th />
+                            <th />
                         </tr>
                     </thead>
                     <tbody>
@@ -36,7 +48,14 @@ class Movies extends Component {
                             <td>{movie.genre.name}</td>
                             <td>{movie.numberInStock}</td>
                             <td>{movie.dailyRentalRate}</td>
-                            <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+                            <td>
+                                <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
+                            </td>
+                            <td><button 
+                            onClick={() => this.handleDelete(movie)} 
+                            className="btn btn-danger btn-sm">
+                                Delete
+                                </button></td>
                         </tr>
                         ))}
 
