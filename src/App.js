74d56c0b4477535d341a9fac1d645490f64a250b
rifqyyusuf12 <rifqy@importir.co>
2022-03-02 17:17:01 +0700

    materi form reactjs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
+import './App.css';
 
 class App extends Component {
 
   render() { 
     return (
-      <main className='container'>
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals}/>
+            <Route path="/not-found" component={NotFound}/>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
  
 export default App;
-
