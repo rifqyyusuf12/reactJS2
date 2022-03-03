3402f5a814d801e6a89f67d27445eab83d7b36bd
rifqyyusuf12 <rifqy@importir.co>
2022-03-03 20:55:41 +0700

    merapihkan file

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,4 +1,3 @@
-import { type } from '@testing-library/user-event/dist/type';
 import React from 'react';
 
 const Input = ({ type, name, label, value, error, onChange }) => {
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Joi from 'joi-browser';
-import { join } from 'lodash';
 import Form from './common/form';
 
 class LoginForm extends Form {
